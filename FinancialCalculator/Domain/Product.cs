e13325baab8737d0cd913bc126604f3fbb55ff2b
dqrtec <dqrtec@hotmail.com>
2024-01-04 00:23:55 -0300

    add domain

---
+++ FinancialCalculator/Domain/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain
+{
+    public class Product
+    {
+        string Name { get; set; }
+        decimal TotalValue { get; set; }
+        DateTime ShoppingDate { get; set; }
+        int NumberOfPart { get; set; }
+
+        public decimal GetPartialValue()
+        {
+            return TotalValue / NumberOfPart;
+        }
+    }
+}
